a744a796378d3e82a626c431745c25547c70f1e3
HP <HP@DESKTOP-ECOEFKE>
2022-09-06 06:29:01 -0700

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            int i =0;
+            Console.WriteLine("Odd Numbers:");
+            for (i = 1; i <= 100; i++) 
+            {
+                if (i % 100 == 0) 
+                {
+                    Console.WriteLine(i +" ");
+                }
+            }
+        }
+    }
+}
